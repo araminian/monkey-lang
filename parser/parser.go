3955637862f0022bea4162a73d6090ceaf372e64
araminian <rmin.aminian@gmail.com>
2024-12-25 15:47:26 +0100

    Chapter 2: Parser - IF/ELSE

--- parser/parser.go
+++ parser/parser.go
@@ -95,6 +95,8 @@ func New(l *lexer.Lexer) *Parser {
 	p.RegisterPrefix(token.MINUS, p.parsePrefixExpression)
 	p.RegisterPrefix(token.TRUE, p.parseBoolean)
 	p.RegisterPrefix(token.FALSE, p.parseBoolean)
+	p.RegisterPrefix(token.LPAREN, p.parseGroupedExpression)
+	p.RegisterPrefix(token.IF, p.parseIfExpression)
 
 	// Register infix parsing functions
 	p.RegisterInfix(token.PLUS, p.parseInfixExpression)
@@ -306,4 +308,69 @@ func (p *Parser) parseInfixExpression(left ast.Expression) ast.Expression {
 // parseBoolean parses a boolean expression
 func (p *Parser) parseBoolean() ast.Expression {
 	return &ast.Boolean{Token: p.curToken, Value: p.curTokenIs(token.TRUE)}
+}
+
+// parseGroupedExpression parses a grouped expression
+func (p *Parser) parseGroupedExpression() ast.Expression {
+	p.nextToken()
+
+	exp := p.parseExpression(LOWEST)
+
+	if !p.expectPeek(token.RPAREN) {
+		return nil
+	}
+
+	return exp
+}
+
+// parseIfExpression parses an if expression
+func (p *Parser) parseIfExpression() ast.Expression {
+	expression := &ast.IfExpression{Token: p.curToken}
+
+	if !p.expectPeek(token.LPAREN) {
+		return nil
+	}
+
+	p.nextToken()
+	expression.Condition = p.parseExpression(LOWEST)
+
+	if !p.expectPeek(token.RPAREN) {
+		return nil
+	}
+
+	if !p.expectPeek(token.LBRACE) {
+		return nil
+	}
+
+	expression.Consequence = p.parseBlockStatement()
+
+	if p.peekTokenIs(token.ELSE) {
+		p.nextToken()
+
+		if !p.expectPeek(token.LBRACE) {
+			return nil
+		}
+
+		expression.Alternative = p.parseBlockStatement()
+	}
+
+	return expression
+}
+
+// parseBlockStatement parses a block statement
+func (p *Parser) parseBlockStatement() *ast.BlockStatement {
+	block := &ast.BlockStatement{Token: p.curToken}
+	block.Statements = []ast.Statement{}
+
+	p.nextToken()
+
+	for !p.curTokenIs(token.RBRACE) && !p.curTokenIs(token.EOF) {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			block.Statements = append(block.Statements, stmt)
+		}
+		p.nextToken()
+	}
+
+	return block
 }
