4bb6a053406c17cbf3166ee915ebabd4f8b9a358
araminian <rmin.aminian@gmail.com>
2024-12-26 17:34:21 +0100

    Chapter 3: Eval - Integer

---
+++ evaluator/evaluator.go
@@ -0,0 +1,26 @@
+package evaluator
+
+import (
+	"github.com/araminian/monkey-lang/ast"
+	"github.com/araminian/monkey-lang/object"
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	case *ast.Program:
+		return evalProgram(node)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+	}
+	return nil
+}
+
+func evalProgram(program *ast.Program) object.Object {
+	var result object.Object
+	for _, statement := range program.Statements {
+		result = Eval(statement)
+	}
+	return result
+}
